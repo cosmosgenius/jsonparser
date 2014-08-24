fbc2f9c4c83bfdced2b90e7d7e397264322182c2
Sharat M R <cosmosgenius@gmail.com>
2014-08-24 18:35:34 +0530

    moduled the code

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 /*jslint node: true */
 'use strict';
 var is   = require('type-is');
+var util = require('util');
+
+var messages = {
+    'contentType' : 'Unexpected Content-Type "%s", expecting "application/json".',
+    'parseError': 'Problems parsing JSON'
+};
+
+function createError(status,message){
+    return {
+        status : status,
+        message : message
+    };
+}
+
+function checkIfJson(req){
+    return is(req, ['json','+json']);
+}
 
 module.exports = function(options) {
     options = options || {};
     var strict = !!options.strictContentType;
     return function(req, res, next) {
-        
         if(strict){
-            var isJSON = is(req, ['json']);
-            if(!isJSON){
-                return next();    
+            if(!checkIfJson(req)){
+                return next(createError(415,util.format(messages.contentType,req.headers['content-type'])));    
             }
         }
         
@@ -18,8 +33,9 @@ module.exports = function(options) {
             req.json = JSON.parse(req.body);
             return next();
         } catch (e) {
-            next();
+            next(createError(400,messages.parseError));
         }
-        
+
     };
 };
+
