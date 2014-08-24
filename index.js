f14420ff09acc7bb90ce8ea0fc7aa04fc5ca293d
Sharat M R <cosmosgenius@gmail.com>
2014-08-24 16:40:56 +0530

    add more tests

--- index.js
+++ index.js
@@ -4,19 +4,22 @@ var is   = require('type-is');
 
 module.exports = function(options) {
     options = options || {};
-    
+    var strict = !!options.strictContentType;
     return function(req, res, next) {
-        var isJSON = is(req, ['json']);
-
-        if(!isJSON){
-            return;
+        
+        if(strict){
+            var isJSON = is(req, ['json']);
+            if(!isJSON){
+                return next();    
+            }
         }
         
         try {
             req.json = JSON.parse(req.body);
             return next();
         } catch (e) {
-
+            next();
         }
+        
     };
 };
--- test/index.js
+++ test/index.js
@@ -2,3 +2,59 @@
 /*jshint expr: true*/
 /*global describe, it, before*/
 'use strict';
+
+var jsonparser = require('../'),
+    request = require('supertest'),
+    bodyparser = require('simple-bodyparser'),
+    http = require('http'),
+    should = require('should');
+
+var server;
+
+function createServer(){
+    var _parser = jsonparser();
+    var _bodyparser = bodyparser();
+
+    return http.createServer(function(req, res){
+        _bodyparser(req, res, function(err){
+            if (err) {
+                res.statusCode = err.status;
+                res.end(err.message);
+                return;
+            }
+            _parser(req, res, function(err){
+                if (err) {
+                    res.statusCode = err.status;
+                    res.end(err.message);
+                    return;
+                }
+                res.end(JSON.stringify(req.json));
+            });
+        });
+    });
+}
+
+describe('jsonparser', function() {
+    before(function(){
+        server = createServer();
+    });
+
+    it('should exist', function() {
+        should.exist(jsonparser);
+    });
+
+    it('should return a function', function() {
+        jsonparser().should.be.a.Function;
+    });
+
+    it('should neglect content type if strictContentType is false');
+
+    it('should fail if content type is not json and strictContentType is true');
+    
+    it('should pass if content type is json and strictContentType is true');
+
+    it('should fail if invalid json data');
+
+    it('should pass for valid json data');
+});
+
