17d7b598b862cd44a221cc941cd1903189b6670b
Sharat M R <cosmosgenius@gmail.com>
2014-08-24 14:36:45 +0530

    added type checking

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 /*jslint node: true */
 'use strict';
+var is   = require('type-is');
 
 module.exports = function(options) {
     options = options || {};
     
     return function(req, res, next) {
+        var isJSON = is(req, ['json']);
+
+        if(!isJSON){
+            return;
+        }
+        
         
     };
 };
